9d2069a833875c9a2637a18021ea8504e0ea866b
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-02-10 05:34:48 +0000

    Deploy v2rayA/v2raya-web to github.com/v2rayA/v2raya-web.git:master

---
+++ precache-manifest.7a20bbfe6dfdeaa92535f8c81260c413.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79fcc3a98e8ae4afc3b5c80635c88ad5",
+    "url": "CNAME"
+  },
+  {
+    "revision": "c71946e756d4f2bd2294",
+    "url": "css/app.bf217451.css"
+  },
+  {
+    "revision": "900a9be8e201edd7c1a3",
+    "url": "css/chunk-vendors.01c3f78a.css"
+  },
+  {
+    "revision": "08107efe8beddfe950a41e26f231b547",
+    "url": "fonts/iconfont.08107efe.eot"
+  },
+  {
+    "revision": "658face7916696624d4de9acfb3d0749",
+    "url": "fonts/iconfont.658face7.ttf"
+  },
+  {
+    "revision": "8a1351233661549938cb65ff4ad258ae",
+    "url": "fonts/iconfont.8a135123.woff2"
+  },
+  {
+    "revision": "b88342fffec0c8c3518683a35c7a4f2a",
+    "url": "fonts/iconfont.b88342ff.woff"
+  },
+  {
+    "revision": "3d1f8fa2f06249540889a7bbe69cf5bb",
+    "url": "fonts/materialdesignicons-webfont.3d1f8fa2.eot"
+  },
+  {
+    "revision": "3e722fd57a6db80ee119f0e2c230ccff",
+    "url": "fonts/materialdesignicons-webfont.3e722fd5.ttf"
+  },
+  {
+    "revision": "4187121a4353440c2a865dbf1bc1901b",
+    "url": "fonts/materialdesignicons-webfont.4187121a.woff2"
+  },
+  {
+    "revision": "fec1b66adcf131415a2511bd77e747cc",
+    "url": "fonts/materialdesignicons-webfont.fec1b66a.woff"
+  },
+  {
+    "revision": "50162f60f2cd5ba81c94bbd8ec32dee0",
+    "url": "img/flags/flag_en.svg"
+  },
+  {
+    "revision": "3a5b5ac1ac98c76572f65c1ddd6dcadf",
+    "url": "img/flags/flag_zh.svg"
+  },
+  {
+    "revision": "25e8bb9ee492495c236722066d740a83",
+    "url": "img/iconfont.25e8bb9e.svg"
+  },
+  {
+    "revision": "5fd6eecaa53e183b0f378fab82e7d108",
+    "url": "img/logo2.5fd6eeca.png"
+  },
+  {
+    "revision": "1952d8b9449cd550328a80aac5dc7f7f",
+    "url": "index.html"
+  },
+  {
+    "revision": "c71946e756d4f2bd2294",
+    "url": "js/app.4dcb9e20.js"
+  },
+  {
+    "revision": "900a9be8e201edd7c1a3",
+    "url": "js/chunk-vendors.2c5ef482.js"
+  },
+  {
+    "revision": "90b5848ee73327b849a31c62c7b41dc6",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "f71d20196d4caf35b6a670db8c70b03d",
+    "url": "robots.txt"
+  }
+]);
